a800b3bc70fa534d369205a01389c1ea91fea1bc
Lucky Rivaldo Mulia Sari <rivaldoliucky300@gmail.com>
2026-05-14 06:11:18 +0700

    BAB3: HobiTime app update

---
+++ src/data/categories.js
@@ -0,0 +1,26 @@
+export const CategoryList = [
+  {
+    id: 1,
+    categoryName: 'Semua',
+  },
+  {
+    id: 2,
+    categoryName: 'Olahraga',
+  },
+  {
+    id: 3,
+    categoryName: 'Gaming',
+  },
+  {
+    id: 4,
+    categoryName: 'Musik',
+  },
+  {
+    id: 5,
+    categoryName: 'Travel',
+  },
+  {
+    id: 6,
+    categoryName: 'Membaca',
+  },
+];
---
+++ src/data/hobbies.js
@@ -0,0 +1,41 @@
+export const HobbyList = [
+  {
+    id: 1,
+    title: 'Belajar Gitar Akustik',
+    category: 'Musik',
+    image:
+      'https://images.unsplash.com/photo-1510915361894-db8b60106cb1?q=80&w=1000&auto=format&fit=crop',
+    createdAt: 'Hari Ini',
+    duration: '1 Jam',
+  },
+
+  {
+    id: 2,
+    title: 'Jogging Pagi',
+    category: 'Olahraga',
+    image:
+      'https://images.unsplash.com/photo-1483721310020-03333e577078?q=80&w=1000&auto=format&fit=crop',
+    createdAt: 'Setiap Pagi',
+    duration: '5 KM',
+  },
+
+  {
+    id: 3,
+    title: 'Push Rank Mobile Game',
+    category: 'Gaming',
+    image:
+      'https://images.unsplash.com/photo-1511512578047-dfb367046420?q=80&w=1000&auto=format&fit=crop',
+    createdAt: 'Malam Hari',
+    duration: '2 Jam',
+  },
+
+  {
+    id: 4,
+    title: 'Membaca Novel',
+    category: 'Membaca',
+    image:
+      'https://images.unsplash.com/photo-1521587760476-6c12a4b040da?q=80&w=1000&auto=format&fit=crop',
+    createdAt: 'Sore Hari',
+    duration: '30 Menit',
+  },
+];
